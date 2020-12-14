326c039794eafb8531dd399ad0b8614c7146dfb3
Felix Vicente <felixvicent1306@gmail.com>
2020-12-14 16:13:02 -0300

    Config statusbat

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-// import { Container } from './styles';
 import Routes from './routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+    <Routes />
+  </>
+);
 
 export default App;
