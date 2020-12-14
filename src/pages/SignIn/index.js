d36eab9c7547d7161ef2fb1c6bcab2930c7e9ef6
Felix Vicente <felixvicent1306@gmail.com>
2020-12-14 16:11:02 -0300

    Styling signup page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
+import PropTypes from 'prop-types';
 
 import Background from '~/components/Background';
-// import Button from '~/components/Button';
 
 import logo from '~/assets/logo.png';
 
@@ -15,7 +15,7 @@ import {
   SignLinkText,
 } from './styles';
 
-const SignIn = () => (
+const SignIn = ({ navigation }) => (
   <Background>
     <Container>
       <Image source={logo} />
@@ -38,11 +38,15 @@ const SignIn = () => (
         <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
       </Form>
 
-      <SignLink onPress={() => {}}>
+      <SignLink onPress={() => navigation.replace('SignUp')}>
         <SignLinkText>Criar conta gratuita</SignLinkText>
       </SignLink>
     </Container>
   </Background>
 );
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({ replace: PropTypes.func }).isRequired,
+};
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
 
 import Background from '~/components/Background';
 
-const SignUp = () => (
+import logo from '~/assets/logo.png';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+const SignUp = ({ navigation }) => (
   <Background>
-    <Text>SignUp</Text>
+    <Container>
+      <Image source={logo} />
+
+      <Form>
+        <FormInput
+          icon="person-outline"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Nome completo"
+        />
+
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Digite seu e-mail"
+        />
+
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+        />
+
+        <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+      </Form>
+
+      <SignLink onPress={() => navigation.replace('SignIn')}>
+        <SignLinkText>Já tem cadastro?</SignLinkText>
+      </SignLink>
+    </Container>
   </Background>
 );
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({ replace: PropTypes.func }).isRequired,
+};
 
 export default SignUp;
