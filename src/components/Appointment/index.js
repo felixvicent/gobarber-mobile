4bd41b16bc99164d44d14fcadc3e1d9fbb1057e1
Felix Vicente <felixvicent1306@gmail.com>
2020-12-16 17:16:17 -0300

    List appointments from api

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
 import { Avatar, Container, Left, Info, Name, Time } from './styles';
 
-const Appointment = () => (
-  <Container>
-    <Left>
-      <Avatar
-        source={{
-          uri:
-            'https://lh3.googleusercontent.com/6dsHVQR36uxpdAdCglKg9gs--PMXAh8snbjoMyMqkWAxlKrLk1ZQOfLLDg6TL38tOOkq-XzEPM3NxojQAQ6rZz8ZOeLXxK3N1wpBkP4lR8_CVa4l7-PlAzf3_H7uYr6Yy8szbXu25OcM7JrdW1eBHxsJGMqpXywMyLHmBlwyRhp3RvxpuBYrATbOJhaYWAQNorDCX6AokeXaf9usmLDyAdNVlljsypcNlchEJVw07YgZA9CNdXwyq2xLR1ZhLfDEwGlTEYRHdXo_jsyj-VqH8HzF-xaWtdPk6odh69mqgHOImVFrUerHB40GRTpxR8j43jpxqivYNMHvm6B-2G7A-11w3nwK3bjG-MzRRA-L3_OuqFp43W7EVVchfQknLUBYAMuuW1Jmr_oEoCNswPZEnTNhV6Kt_Pe4oFh_--R3L-3HZrTtfUjuZeUPStE620KfYyAedjD95pybH14d5oCCk-sBe4vMbfzfi_rHIfDC8I7LWLI9-ZGFwYuKu3HuMQQz8IiiSEZFWbHeQSqUlcqdEhC4vD5rrtc1UcqBNcoVRdp7QB755gBBfyuJdOD7HX2aVUe8FOJnqaV8JmW8iHa1jbiyN8emb5hLKgPBQAf9jazTB9dctGu4KtCmpvC1I6DHL-LZcCqXyhmyBwAxVpqAGBl50WteENBDBCFJUwUSEjsp1IuhVLGYD4yrxGtu=w621-h828-no?authuser=0',
-        }}
-      />
-      <Info>
-        <Name>Félix Vicente</Name>
-        <Time>em 3 horas</Time>
-      </Info>
-    </Left>
+const Appointment = ({ data, onCancel }) => {
+  const dateParsed = useMemo(
+    () =>
+      formatRelative(parseISO(data.date), new Date(), {
+        locale: pt,
+        addSufix: true,
+      }),
+    [data.date]
+  );
 
-    <TouchableOpacity onPress={() => {}}>
-      <Icon name="event-busy" size={20} color="#f64c75" />
-    </TouchableOpacity>
-  </Container>
-);
+  return (
+    <Container past={data.past}>
+      <Left>
+        <Avatar
+          source={{
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : 'https://ui-avatars.com/api/?name=F%C3%A9lix+Vicente',
+          }}
+        />
+        <Info>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
+        </Info>
+      </Left>
+
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
+    </Container>
+  );
+};
+
+Appointment.propTypes = {
+  data: PropTypes.shape({
+    provider: PropTypes.shape({
+      name: PropTypes.string,
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    }),
+    date: PropTypes.string,
+    past: PropTypes.bool,
+    cancelable: PropTypes.bool,
+    canceled_at: PropTypes.string,
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
 
 export default Appointment;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '~/services/api';
 
 import Appointment from '~/components/Appointment';
 import Background from '~/components/Background';
 
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
+const Dashboard = () => {
+  const [appointments, setAppointments] = useState([]);
 
-const Dashboard = () => (
-  <Background>
-    <Container>
-      <Title>Agendamentos</Title>
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
 
-      <List
-        data={data}
-        keyExtractor={item => String(item)}
-        renderItem={({ item }) => <Appointment data={item} />}
-      />
-    </Container>
-  </Background>
-);
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    console.tron.log('Aqui');
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? { ...appointment, canceled_at: response.data.canceled_at }
+          : appointment
+      )
+    );
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
+        />
+      </Container>
+    </Background>
+  );
+};
 
 export default Dashboard;
