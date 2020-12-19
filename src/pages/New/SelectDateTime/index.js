80da1e5e8be6018c701f13ba3eaa643f456aa606
Felix Vicente <felixvicent1306@gmail.com>
2020-12-19 13:01:41 -0300

    Choose Hour

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,19 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import api from '~/services/api';
 
 import DateInput from '~/components/DateInput';
 import Background from '~/components/Background';
 
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
 
-const SelectDateTime = () => {
+const SelectDateTime = ({ navigation, route }) => {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const { provider } = route.params;
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: { date: date.getTime() },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', { provider, time });
+  }
+
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
+};
+
+SelectDateTime.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      provider: PropTypes.shape({
+        id: PropTypes.number,
+      }),
+    }),
+  }).isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
 };
 
 export default SelectDateTime;
